09b972dae5dd035c5c2b0bb0a97eb95db58e36ae
Kev <kev162020@gmail.com>
2023-04-16 16:12:38 -0600

    New users/include now search

--- static/users/script.js
+++ static/users/script.js
@@ -1,18 +1,32 @@
 //AJAX fot requests
 const request = new XMLHttpRequest();
 const ip = 'http://127.0.0.1:8000/'
-const endpoint = 'v1/users'
-const URL = ip + endpoint
 
 
 //Elements
-const reload = document.querySelector('.reload');
-const table_header = document.querySelector('thead')
-const table_body = document.querySelector('tbody')
+//Elements to show all users
+const btn_all_users = document.querySelector('.all-users');
+const table_container = document.querySelector('.table-container')
+const table_body = document.querySelector('.table-container tbody')
+
+
+//Elements to search a user by id
+const btn_search = document.querySelector('.search')
+
+const table_search = document.querySelector('.table-search')
+const table_body_search = document.querySelector('.table-search tbody')
+
+const form_container = document.querySelector('.form-container')
+const btn_send = document.querySelector('.send');
+const data = document.querySelector('.form-container input');
 
 
 //functions
+//functions to all users
 function getUsers() {
+    const endpoint = 'v1/users'
+    const URL = ip + endpoint
+    
     request.open('GET', URL);
     request.responseType = 'json';
     request.onload = function () {
@@ -28,26 +42,13 @@ function getUsers() {
 
 function showUsers(users) {
     
-    const titles = document.createElement('tr');
-    titles.classList.add('titles')
-
-    const title1 = document.createElement('th');
-    title1.innerText = 'ID';
-
-    const title2 = document.createElement('th');
-    title2.innerText = 'Name';
-
-    const title3 = document.createElement('th');
-    title3.innerText = 'Group ID';
-
-    titles.appendChild(title1)
-    titles.appendChild(title2)
-    titles.appendChild(title3)
-    table_header.appendChild(titles)
-
-
     for (user of users){
         const row = document.createElement('tr');
+
+        const checkContainer = document.createElement('td')
+
+        const checkUser = document.createElement('input')
+        checkUser.type = 'checkbox'
 
         const ID = document.createElement('td');
         ID.innerText = user.id;
@@ -58,6 +59,9 @@ function showUsers(users) {
         const groupID = document.createElement('td');
         groupID.innerText = user.group;
 
+        checkContainer.appendChild(checkUser)
+
+        row.appendChild(checkContainer)
         row.appendChild(ID)
         row.appendChild(name)
         row.appendChild(groupID)
@@ -66,6 +70,72 @@ function showUsers(users) {
 }
 
 
+//functions to show a user by id
+function getUser() {
+    const endpoint = 'v1/users/'
+    const URL = ip + endpoint
+
+    id = data.value;
+    console.log(id);
+
+    request.open('GET', URL + id);
+    request.responseType = 'json';
+    request.onload = function () {
+        if (this.status == 200){
+            var users = this.response;
+            showUser(users.detail)
+        } else {
+            console.log(`An error occurred during your request! Code: ${request.status}`);
+            var user = {name: 'No exists', id: 'No exists', group: 'No exists'}
+            showUser(user)
+        }
+    }
+    request.send();
+}
+
+
+function showUser(user) {
+    const old_row = document.querySelector('.old-row')
+    
+    const row = document.createElement('tr');
+
+    const ID = document.createElement('td');
+    ID.innerText = user.id;
+
+    const name = document.createElement('td');
+    name.innerText = user.name;
+
+    const groupID = document.createElement('td');
+    groupID.innerText = user.group;
+
+    row.appendChild(ID)
+    row.appendChild(name)
+    row.appendChild(groupID)
+    row.classList.add('old-row')
+
+    table_body_search.replaceChild(row, old_row)
+}
+
+
+//functions to activate or deactivate views
+function activateAllUsers() {
+    table_container.style.display='flex'
+
+    form_container.style.display='none'
+    table_search.style.display='none'
+}
+
+
+function activateSearchUser() {
+    table_container.style.display='none'
+
+    form_container.style.display='flex'
+    table_search.style.display='flex'
+}
+
+
 //listeners
 window.addEventListener('load', getUsers);
-reload.addEventListener('click', _ => location.reload())
+btn_all_users.addEventListener('click', activateAllUsers)
+btn_search.addEventListener('click', activateSearchUser)
+btn_send.addEventListener('click', getUser);
--- webpage/webpage.py
+++ webpage/webpage.py
@@ -1,9 +1,14 @@
 #fastapi
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 
 
 webpage = APIRouter()
+
+@webpage.get(path='/')
+def returnHome():
+    return RedirectResponse("http://127.0.0.1:8000/home")
+
 
 @webpage.get(path='/home')
 def home():
@@ -13,8 +18,3 @@ def home():
 @webpage.get(path='/users')
 def users():
     return FileResponse(r".\static\users\index.html")
-
-
-@webpage.get(path='/users/search')
-def users():
-    return FileResponse(r".\static\users\search\index.html")
